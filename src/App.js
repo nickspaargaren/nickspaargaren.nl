86e4d2c4cb72165cb668e5dc4f12994dc9a4e170
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2020-04-02 10:17:52 +0200

    rel=noopener

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="inhoud">
-      <a href="https://www.linkedin.com/in/nickspaargaren" target="_blank" rel="nofollow"><i className="fab fa-linkedin"></i><span>Nick Spaargaren</span></a>
+      <a href="https://www.linkedin.com/in/nickspaargaren" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i><span>Nick Spaargaren</span></a>
     </div>
   );
 }
