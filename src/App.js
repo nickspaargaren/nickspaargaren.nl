6dd9ccf800bed99bdc9bb7b1a6e5c4353d95b2ff
Nick Spaargaren <>
2020-04-15 11:38:04 +0200

    Github+Dribbble toegevoegd en nieuw styling

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="inhoud">
-      <a href="https://www.linkedin.com/in/nickspaargaren" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i><span>Nick Spaargaren</span></a>
+      <div className="titel">
+        <h1>Nick Spaargaren</h1>
+        <h2>Designer & Front-End Developer</h2>
+      </div>
+      <a href="https://github.com/nickspaargaren" target="_blank" className="github" rel="noopener noreferrer"><i className="fab fa-github"></i><span><strong>Github</strong>Nick Spaargaren</span></a>
+      <a href="https://www.linkedin.com/in/nickspaargaren" target="_blank" className="linkedin" rel="noopener noreferrer"><i className="fab fa-linkedin"></i><span><strong>Linkedin</strong>Nick Spaargaren</span></a>
+      <a href="https://dribbble.com/nickspaargaren" target="_blank" className="dribbble" rel="noopener noreferrer"><i className="fab fa-dribbble"></i><span><strong>Dribbble</strong>Nick Spaargaren</span></a>
     </div>
   );
 }
