01061370271ac781f270bb9a8a484ecd6b2bdc14
Nick Spaargaren <>
2020-06-03 22:40:33 +0200

    indent en comp. zonder class styling

--- src/components/socialblok.js
+++ src/components/socialblok.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 function Socialblok(item) {
-  return <a className={item.platform} rel="noopener noreferrer" href={item.link} target="_blank" >
-          <i className={`fab fa-${item.platform}`}></i>
-          <span><strong>{item.platform}</strong>Nick Spaargaren</span>
-        </a>;
+  return (
+    <a className={item.platform} rel="noopener noreferrer" href={item.link} target="_blank" >
+      <i className={`fab fa-${item.platform}`}></i>
+      <span><strong>{item.platform}</strong>Nick Spaargaren</span>
+    </a>
+  );
 }
 
 export default Socialblok;
