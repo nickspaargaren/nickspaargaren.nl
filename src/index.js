670bae59805c9eaaf56c6d5541749c7106239b4a
Nick <nick@Nicks-MacBook-Pro.local>
2020-07-06 19:17:31 +0200

    Grid.

--- src/index.js
+++ src/index.js
@@ -6,15 +6,17 @@ import Socialblok from './components/socialblok.js';
 
 function App() {
   return (
-    <div className="inhoud">
+  <div className="inhoud">
       <div className="titel">
         <h1>Nick Spaargaren</h1>
         <h2>Designer & Front-End Developer</h2>
       </div>
-      <Socialblok platform="dribbble" link="https://dribbble.com/nickspaargaren" />
-      <Socialblok platform="github" link="https://github.com/nickspaargaren" />
-      <Socialblok platform="youtube" link="https://www.youtube.com/watch?v=zDiSkH9PPJg&list=PLrZcPERRIctdZztCmNMHQ11aOne2yYOX3" />
-      <Socialblok platform="linkedin" link="https://www.linkedin.com/in/nickspaargaren" />
+      <div className="socialmedia">
+        <Socialblok platform="dribbble" link="https://dribbble.com/nickspaargaren" />
+        <Socialblok platform="github" link="https://github.com/nickspaargaren" />
+        <Socialblok platform="youtube" link="https://www.youtube.com/watch?v=zDiSkH9PPJg&list=PLrZcPERRIctdZztCmNMHQ11aOne2yYOX3" />
+        <Socialblok platform="linkedin" link="https://www.linkedin.com/in/nickspaargaren" />
+      </div>
     </div>
   );
 }
