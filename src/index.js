910b4fa9dd0fb50123ffb35868a4eb2a2dd9533c
Nick <nickspaargaren@users.noreply.github.com>
2020-07-12 20:30:51 +0200

    Code splitting

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import './index.css';
-import Socialblok from './components/socialblok.js';
+const Socialblok = React.lazy(() => import('./components/socialblok')); 
 
 function App() {
   return (
@@ -11,12 +11,14 @@ function App() {
         <h1>Nick Spaargaren</h1>
         <h2>Designer & Front-End Developer</h2>
       </div>
-      <div className="socialmedia">
-        <Socialblok platform="dribbble" link="https://dribbble.com/nickspaargaren" />
-        <Socialblok platform="github" link="https://github.com/nickspaargaren" />
-        <Socialblok platform="youtube" link="https://www.youtube.com/watch?v=zDiSkH9PPJg&list=PLrZcPERRIctdZztCmNMHQ11aOne2yYOX3" />
-        <Socialblok platform="linkedin" link="https://www.linkedin.com/in/nickspaargaren" />
-      </div>
+      <Suspense fallback={<div>Laden...</div>}>
+        <div className="socialmedia">
+          <Socialblok platform="dribbble" link="https://dribbble.com/nickspaargaren" />
+          <Socialblok platform="github" link="https://github.com/nickspaargaren" />
+          <Socialblok platform="youtube" link="https://www.youtube.com/watch?v=zDiSkH9PPJg&list=PLrZcPERRIctdZztCmNMHQ11aOne2yYOX3" />
+          <Socialblok platform="linkedin" link="https://www.linkedin.com/in/nickspaargaren" />
+        </div>
+      </Suspense>
     </div>
   );
 }
