e86a79472eab806ce453ed09782032dd05798ae1
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2020-10-07 17:35:11 +0200

    Compacter -component

--- src/components/social.js
+++ src/components/social.js
@@ -2,13 +2,6 @@ import React from 'react';
 import styles from './social.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-const SocialBlok = ({platform, socialclass, link}) => (
-  <a className={`${styles.blok} ${socialclass}`} rel="noopener noreferrer" href={link} target="_blank" >
-    <FontAwesomeIcon icon={["fab", `${platform.toLowerCase()}`]} />
-    <span><strong>{platform}</strong>Nick Spaargaren</span>
-  </a>
-)
-
 const SocialList = [
   {id: 1, platform: 'Dribbble', class: styles.dribbble, link: 'https://dribbble.com/nickspaargaren'},
   {id: 2, platform: 'Github', class: styles.github, link: 'https://github.com/nickspaargaren'},
@@ -16,10 +9,13 @@ const SocialList = [
   {id: 4, platform: 'Linkedin', class: styles.linkedin, link: 'https://www.linkedin.com/in/nickspaargaren'}
 ];
 
-const Socials = () => (
+const Socials = ({platform, socialclass, link}) => (
   <div className={styles.houder}>
     {SocialList.map(social =>
-      <SocialBlok key={social.id} platform={social.platform} socialclass={social.class} link={social.link} />
+        <a key={social.id} className={`${styles.blok} ${social.class}`} rel="noopener noreferrer" href={social.link} target="_blank" >
+          <FontAwesomeIcon icon={["fab", `${social.platform.toLowerCase()}`]} />
+          <span><strong>{social.platform}</strong>Nick Spaargaren</span>
+        </a>
     )}
   </div>
 )
