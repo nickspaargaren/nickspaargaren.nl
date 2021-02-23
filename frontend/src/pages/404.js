b647e3935db0d9a5cf259f9de103c05688c2a73f
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2021-02-23 21:53:58 +0100

    wip portfolio - Styled components - Globalstyles - 404 - Nav in layout - Gatsby update - Meer styling

--- frontend/src/pages/404.js
+++ frontend/src/pages/404.js
@@ -1,4 +1,11 @@
+import React from "react"
+import Layout from "../layout/layout"
+
 const vierNulVier = () => {
-  return 404
+  return (
+    <Layout>
+      <div className="inhoud">404</div>
+    </Layout>
+  )
 }
 export default vierNulVier
