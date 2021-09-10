d4b5d90a3fa26794e0956f500b24fa35334317d7
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2021-09-10 12:04:37 +0200

    Reusable components - Menu - User

---
+++ frontend/src/components/navigatie/Menu.tsx
@@ -0,0 +1,104 @@
+import React, {useEffect, useRef} from 'react';
+import styled from 'styled-components';
+import {Link} from 'gatsby';
+
+const StyledMenu = styled.div`
+  margin: auto 0 auto auto;
+  position: relative;
+
+  ul.menu {
+    margin: 0;
+    padding: 4px 0;
+    background: #eee;
+    border-radius: 5px;
+    text-align: right;
+    list-style: none;
+    display: flex;
+    position: relative;
+    box-shadow: 0 2px 7px -1px rgb(0 0 0 / 15%) inset;
+  }
+  ul.menu li a {
+    display: block;
+    position: relative;
+    margin: 0 5px;
+    padding: 6px 15px 7px;
+    text-decoration: none;
+    z-index: 2;
+    font-size: 15px;
+  }
+  .hover {
+    top: 4px;
+    bottom: 4px;
+    background: #fff;
+    box-shadow: 0 1px 0 0 rgba(0, 0, 0, 0.15);
+    position: absolute;
+    transition: 0.3s all cubic-bezier(0.65, 0.05, 0.36, 1);
+    border-radius: 5px;
+    z-index: 1;
+  }
+
+  @media (max-width: 760px) {
+    margin: 10px 0 0;
+    ul.menu {
+      justify-content: center;
+    }
+  }
+`;
+
+const Menu = () => {
+  const hoverRef = useRef(null);
+
+  const menuItemRef1 = useRef(null);
+  const menuItemRef2 = useRef(null);
+
+  const hover = e => {
+    hoverRef.current.style.left = `${e.target.offsetLeft}px`;
+    hoverRef.current.style.width = `${e.target.clientWidth}px`;
+  };
+
+  const leave = () => {
+    if (menuItemRef1.current.classList.contains('active')) {
+      hoverRef.current.style.left = `${menuItemRef1.current.offsetLeft}px`;
+      hoverRef.current.style.width = `${menuItemRef1.current.clientWidth}px`;
+    }
+    if (menuItemRef2.current.classList.contains('active')) {
+      hoverRef.current.style.left = `${menuItemRef2.current.offsetLeft}px`;
+      hoverRef.current.style.width = `${menuItemRef2.current.clientWidth}px`;
+    }
+  };
+
+  useEffect(() => {
+    leave();
+  });
+
+  return (
+    <StyledMenu>
+      <ul className="menu">
+        <li>
+          <Link
+            activeClassName="active"
+            onMouseEnter={hover}
+            onMouseLeave={leave}
+            ref={menuItemRef1}
+            to="/portfolio/"
+          >
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link
+            activeClassName="active"
+            onMouseEnter={hover}
+            onMouseLeave={leave}
+            ref={menuItemRef2}
+            to="/portfolio/drone/"
+          >
+            Drone videos
+          </Link>
+        </li>
+      </ul>
+      <div className="hover" ref={hoverRef}></div>
+    </StyledMenu>
+  );
+};
+export default Menu;
--- frontend/src/components/navigatie/Nav.tsx
+++ frontend/src/components/navigatie/Nav.tsx
@@ -1,167 +1,44 @@
-import React, {useEffect, useRef} from 'react';
-import {Link} from 'gatsby';
-import {StaticImage} from 'gatsby-plugin-image';
-import {useSiteMetadata} from '../../data/hooks/algemeen';
+import React from 'react';
 import styled from 'styled-components';
+import Menu from './Menu';
+import User from '../User';
+import {useSiteMetadata} from '../../data/hooks/algemeen';
+import {StaticImage} from 'gatsby-plugin-image';
 
 const StyledNav = styled.div`
   background: #fff;
   color: #292a2c;
   display: flex;
+  justify-content: space-between;
   z-index: 2;
   position: relative;
   padding: 20px;
   box-shadow: 0 6px 35px -9px rgb(0 0 0 / 20%);
 
-  > div {
-    margin: auto 0;
-  }
-  > div a {
+  a {
     color: inherit;
     text-decoration: none;
-  }
-  > .info {
-    display: flex;
-  }
-  > .info .gatsby-image-wrapper {
-    margin: auto 10px auto 0;
-    border-radius: 50px;
-  }
-
-  .switch {
-    overflow: hidden;
-    height: 17px;
-  }
-  .switch > div {
-    position: relative;
-    top: 0;
-    transition: 0.3s all ease;
-  }
-
-  > .info:hover .switch > div {
-    top: -17px;
-  }
-
-  .menuHouder {
-    margin: auto 0 auto auto;
-    position: relative;
-  }
-  .menuHouder ul.menu {
-    margin: 0;
-    padding: 4px 0;
-    background: #eee;
-    border-radius: 5px;
-    text-align: right;
-    list-style: none;
-    display: flex;
-    position: relative;
-    box-shadow: 0 2px 7px -1px rgb(0 0 0 / 15%) inset;
-  }
-  .menuHouder ul.menu li a {
-    display: block;
-    position: relative;
-    margin: 0 5px;
-    padding: 6px 15px 7px;
-    text-decoration: none;
-    z-index: 2;
-    font-size: 15px;
-  }
-  .menuHouder .hover {
-    top: 4px;
-    bottom: 4px;
-    background: #fff;
-    box-shadow: 0 1px 0 0 rgba(0, 0, 0, 0.15);
-    position: absolute;
-    transition: 0.3s all cubic-bezier(0.65, 0.05, 0.36, 1);
-    border-radius: 5px;
-    z-index: 1;
-  }
-
-  @media (max-width: 760px) {
-    flex-direction: column;
-    .menuHouder {
-      margin: 10px 0 0;
-    }
-    .menuHouder ul.menu {
-      justify-content: center;
-    }
   }
 `;
 
 const Nav = () => {
   const {naam, functie, email} = useSiteMetadata();
-
-  const hoverRef = useRef(null);
-
-  const menuItemRef1 = useRef(null);
-  const menuItemRef2 = useRef(null);
-
-  const hover = e => {
-    hoverRef.current.style.left = `${e.target.offsetLeft}px`;
-    hoverRef.current.style.width = `${e.target.clientWidth}px`;
-  };
-
-  const leave = () => {
-    if (menuItemRef1.current.classList.contains('active')) {
-      hoverRef.current.style.left = `${menuItemRef1.current.offsetLeft}px`;
-      hoverRef.current.style.width = `${menuItemRef1.current.clientWidth}px`;
-    }
-    if (menuItemRef2.current.classList.contains('active')) {
-      hoverRef.current.style.left = `${menuItemRef2.current.offsetLeft}px`;
-      hoverRef.current.style.width = `${menuItemRef2.current.clientWidth}px`;
-    }
-  };
-
-  useEffect(() => {
-    leave();
-  });
-
   return (
     <StyledNav>
-      <div className="info">
-        <StaticImage
-          src="../../images/nick-spaargaren.jpeg"
-          width={37}
-          height={37}
-          alt={`${'Profielfoto'} ${naam}`}
-        />
-        <div>
-          <strong>{naam}</strong>
-          <div className="switch">
-            <div className="klein">{functie}</div>
-            <div className="klein">
-              <a href={`mailto:${email}`}>{email}</a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="menuHouder">
-        <ul className="menu">
-          <li>
-            <Link
-              activeClassName="active"
-              onMouseEnter={hover}
-              onMouseLeave={leave}
-              ref={menuItemRef1}
-              to="/portfolio/"
-            >
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link
-              activeClassName="active"
-              onMouseEnter={hover}
-              onMouseLeave={leave}
-              ref={menuItemRef2}
-              to="/portfolio/drone/"
-            >
-              Drone videos
-            </Link>
-          </li>
-        </ul>
-        <div className="hover" ref={hoverRef}></div>
-      </div>
+      <User
+        title={naam}
+        subtitle={functie}
+        subtitleHover={email}
+        image={
+          <StaticImage
+            src="../images/nick-spaargaren.jpeg"
+            width={37}
+            height={37}
+            alt={`${'Profielfoto'} ${naam}`}
+          />
+        }
+      />
+      <Menu />
     </StyledNav>
   );
 };
