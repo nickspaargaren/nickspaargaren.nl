cafc9d051971129b1b89fbb3bd0073444c5dc3f0
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2021-09-10 12:04:37 +0200

    Reusable components - Menu - User

---
+++ frontend/src/components/User.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyleUser = styled.div`
+  display: flex;
+  .gatsby-image-wrapper {
+    margin: auto 10px auto 0;
+    border-radius: 50px;
+  }
+
+  .switch {
+    overflow: hidden;
+    height: 17px;
+  }
+
+  .switch > div {
+    position: relative;
+    top: 0;
+    transition: 0.3s all ease;
+  }
+
+  &:hover .switch > div {
+    top: -17px;
+  }
+`;
+
+interface UserProps {
+  title: string;
+  subtitle: string;
+  subtitleHover: string;
+  image?: object;
+}
+
+const User = ({title, subtitle, subtitleHover, image}: UserProps) => {
+  return (
+    <StyleUser>
+      {image}
+      <div>
+        <strong>{title}</strong>
+        <div className="switch">
+          <div className="klein">{subtitle}</div>
+          <div className="klein">
+            <a href={`mailto:${subtitleHover}`}>{subtitleHover}</a>
+          </div>
+        </div>
+      </div>
+    </StyleUser>
+  );
+};
+
+export default User;
