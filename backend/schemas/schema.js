50815e5e0c1c1f65970b2d68c9b5013678b6d5b8
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2022-01-12 17:46:46 +0100

    Add Stats scheme

--- backend/schemas/schema.js
+++ backend/schemas/schema.js
@@ -6,6 +6,7 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 import portfolio from './portfolio.js'
 import skills from './skills.js'
+import stats from './stats.js'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -15,6 +16,7 @@ export default createSchema({
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
     portfolio,
-    skills
+    skills,
+    stats
   ]),
 })
