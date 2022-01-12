1f479051d9ae65edb33dceee7ae0b75d7466e3e9
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2022-01-12 17:46:46 +0100

    Add Stats scheme

---
+++ backend/schemas/stats.js
@@ -0,0 +1,27 @@
+export default {
+  name: 'stats',
+  title: 'Stats',
+  type: 'document',
+  fields: [
+    {
+      name: 'titel',
+      title: 'Titel',
+      type: 'string',
+    },
+    {
+      name: 'subtitel',
+      title: 'Subtitel',
+      type: 'string',
+    },
+    {
+      name: 'amount',
+      title: 'Amount',
+      type: 'number',
+    },
+    {
+      name: 'icoon',
+      title: 'Icoon',
+      type: 'string',
+    },
+  ]
+}
