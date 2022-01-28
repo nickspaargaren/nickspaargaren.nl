03026d48e3cfdc2723b35695d09547f016d3285a
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2022-01-28 21:37:46 +0100

    Add TagList component

--- frontend/src/components/PortfolioItem.tsx
+++ frontend/src/components/PortfolioItem.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { GatsbyImage } from "gatsby-plugin-image";
 import styled from "styled-components";
 import { BsChevronUp } from "react-icons/bs";
+import Taglist from "./Taglist";
 
 const StyledPortfolioItem = styled.div`
   position: relative;
@@ -38,29 +39,8 @@ const StyledPortfolioItem = styled.div`
     overflow: hidden;
   }
 
-  .description ul.tags {
-    list-style: none;
-    margin: 0;
-    padding: 0;
-    display: flex;
-    justify-content: center;
-    height: 0;
-    opacity: 0;
-    transition: 0.3s all ease;
-  }
-
-  .description .cm-specialist {
-    opacity: 0;
-    margin-bottom: 15px;
-    transition: 0.3s all ease;
-  }
-
   &:hover {
-    .description .cm-specialist {
-      opacity: 0.65;
-    }
-
-    .description ul.tags {
+    .description ul {
       height: 47px;
       opacity: 1;
       padding-bottom: 30px;
@@ -70,80 +50,10 @@ const StyledPortfolioItem = styled.div`
       height: 0;
       opacity: 0;
     }
-    .description ul.tags li {
+    .description ul li {
       top: 0;
       opacity: 1;
     }
-  }
-  .description ul.tags li {
-    display: inline-block;
-    margin: 10px 5px 0 5px;
-    position: relative;
-    transition: 0.3s all ease;
-  }
-
-  .description ul.tags li {
-    top: 30px;
-    opacity: 0;
-  }
-  .description ul.tags li:nth-child(2) {
-    transition: 0.4s;
-  }
-  .description ul.tags li:nth-child(3) {
-    transition: 0.5s;
-  }
-  .description ul.tags li:nth-child(4) {
-    transition: 0.5s;
-  }
-  .description ul.tags li:nth-child(5) {
-    transition: 0.6s;
-  }
-
-  .description ul.tags li span {
-    position: absolute;
-    bottom: 100%;
-    background: #221e25;
-    color: #fff;
-    line-height: 1;
-    white-space: nowrap;
-    font-size: 14px;
-    padding: 7px;
-    position: absolute;
-    transition: 0.3s all ease;
-    left: 50%;
-    transform: translateX(-50%);
-    opacity: 0;
-    pointer-events: none;
-  }
-  .description ul.tags li span::before {
-    border: solid;
-    border-color: #221e25 transparent;
-    border-width: 6px 6px 0px 6px;
-    bottom: -4px;
-    content: "";
-    position: absolute;
-    transition: 0.3s all ease;
-    left: 50%;
-    transform: translateX(-50%);
-  }
-  .description ul.tags li:hover span {
-    opacity: 1;
-    pointer-events: all;
-    bottom: 115%;
-  }
-  .description ul.tags .gatsby-image-wrapper {
-    position: relative;
-  }
-  .description ul.tags .gatsby-image-wrapper::after {
-    content: "";
-    position: absolute;
-    z-index: 0;
-    top: -10px;
-    right: -10px;
-    bottom: -10px;
-    left: -10px;
-    border-radius: 12px;
-    transition: 0.3s all ease;
   }
 `;
 
@@ -174,17 +84,7 @@ const PortfolioItem = ({
       <h2>{title}</h2>
       <p>{subtitle}</p>
       <BsChevronUp />
-      <ul className="tags">
-        {skillsused.map((skill, key) => (
-          <li key={key}>
-            <GatsbyImage
-              image={skill.image.asset.gatsbyImageData}
-              alt={skill.title}
-            />
-            <span>{skill.title}</span>
-          </li>
-        ))}
-      </ul>
+      <Taglist tags={skillsused} />
     </div>
   </StyledPortfolioItem>
 );
---
+++ frontend/src/components/Taglist.tsx
@@ -0,0 +1,93 @@
+import React from "react";
+import styled from "styled-components";
+import { GatsbyImage } from "gatsby-plugin-image";
+
+const StyledTaglist = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  height: 0;
+  opacity: 0;
+  transition: 0.3s all ease;
+
+  li {
+    display: inline-block;
+    margin: 10px 10px 0 0;
+    position: relative;
+    transition: 0.3s all ease;
+  }
+
+  li span {
+    position: absolute;
+    bottom: 100%;
+    background: #221e25;
+    color: #fff;
+    line-height: 1;
+    white-space: nowrap;
+    font-size: 14px;
+    padding: 7px;
+    position: absolute;
+    transition: 0.3s all ease;
+    left: 50%;
+    transform: translateX(-50%);
+    opacity: 0;
+    pointer-events: none;
+  }
+
+  li span::before {
+    border: solid;
+    border-color: #221e25 transparent;
+    border-width: 6px 6px 0px 6px;
+    bottom: -4px;
+    content: "";
+    position: absolute;
+    transition: 0.3s all ease;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  li:hover span {
+    opacity: 1;
+    pointer-events: all;
+    bottom: 115%;
+  }
+
+  .gatsby-image-wrapper {
+    position: relative;
+  }
+
+  .gatsby-image-wrapper::after {
+    content: "";
+    position: absolute;
+    z-index: 0;
+    top: -10px;
+    right: -10px;
+    bottom: -10px;
+    left: -10px;
+    border-radius: 12px;
+    transition: 0.3s all ease;
+  }
+`;
+
+type TaglistType = {
+  tags: [
+    {
+      title: string;
+      image: any;
+    }
+  ];
+};
+
+const Taglist = ({ tags }: TaglistType) => (
+  <StyledTaglist>
+    {tags.map(({ title, image }, key: any) => (
+      <li key={key}>
+        <GatsbyImage image={image.asset.gatsbyImageData} alt={title} />
+        <span>{title}</span>
+      </li>
+    ))}
+  </StyledTaglist>
+);
+export default Taglist;
