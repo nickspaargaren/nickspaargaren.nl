261d96f271cfb5b11fa07b20ec82053e87691985
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2022-05-16 21:27:13 +0200

    Add base url to cypress setup

--- frontend/cypress/integration/setFavorites.spec.js
+++ frontend/cypress/integration/setFavorites.spec.js
@@ -2,10 +2,11 @@
 
 describe("Set favorites test", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:3000/portfolio");
+    cy.visit("/portfolio");
   });
 
   it("can add one item to favorites", () => {
+    cy.wait(4000);
     cy.get("li.tag.reset").should("not.exist");
     cy.get(":nth-child(2) > .opties > .favorite > .icon").click();
     cy.get("li.tag.reset").should("have.text", "Alle verwijderen");
