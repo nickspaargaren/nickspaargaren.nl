e6444453d222ff91bda09f64187a310d1c499ccb
Nick Spaargaren <20847106+nickspaargaren@users.noreply.github.com>
2024-09-22 14:21:25 +0200

    Update ESLint to v9 (#88)
    * Update ESLint to v9
    
    * Refactor localStorage update logic to comply with ESLint recommended rules
    
    * Remove prettier commands
    - ESLint will run this automatically
    
    * Remove ESLint config v8
    
    ---------
    
    Co-authored-by: Nick Spaargaren <nickspaargaren@users.noreply.github.com>

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,23 @@
+import eslint from "@eslint/js";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+  eslintPluginPrettierRecommended,
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "prettier/prettier": "warn",
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+    },
+  },
+);
