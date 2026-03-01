bb442dc3c9429d06a61097c5b430cb0192db7bb5
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2026-03-01 16:26:27 +0100

    Replace tseslint.config with eslint defineConfig

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -2,8 +2,9 @@ import eslint from "@eslint/js";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 import simpleImportSort from "eslint-plugin-simple-import-sort";
 import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
 
-export default tseslint.config(
+export default defineConfig(
   eslint.configs.recommended,
   ...tseslint.configs.strict,
   ...tseslint.configs.stylistic,
