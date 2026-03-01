a7fb8609764f6d65a60a75ef22daee6df91ed50a
Nick Spaargaren <nickspaargaren@users.noreply.github.com>
2026-03-01 15:21:12 +0100

    Add HTML validation e2e test

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from "cypress";
+import htmlvalidate from "cypress-html-validate/plugin";
 
 export default defineConfig({
   e2e: {
+    setupNodeEvents(on) {
+      htmlvalidate.install(on, {
+        rules: {
+          "require-sri": "off",
+          "valid-id": "off",
+        },
+      });
+    },
     supportFile: false,
     baseUrl: "http://localhost:9000/",
   },
